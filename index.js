9ab8fbbb522a5bd5b3c83b210962c71e7ba613f7
BrianLee <briankigondu@gmail.com>
2017-01-27 21:26:00 +0300

    First Commit
    Added the main files and template

---
+++ index.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+/*
+	get full chart with data, store in text file, read and extract songs from data
+ */
+
+
+var billboard = require("billboard-top-100").getChart;
+
+billboard('hot-100', function(songs,err){
+	if(err) console.error(err);
+	var data = JSON.stringify(songs);
+
+	fs.writeFile('data.txt',data,'utf-8',function(err){
+		if(err) console.error(err);
+		console.log('Chart was saved, master')
+	})
+	
+})
+
+
