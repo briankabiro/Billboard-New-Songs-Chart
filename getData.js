720ca0e8f94e11d7abc49d7c14380d2f101f5bb2
BrianLee <briankigondu@gmail.com>
2017-04-14 21:38:26 +0300

    tried await

--- getData.js
+++ getData.js
@@ -1,2 +1,22 @@
 var fs = require('fs');
+var await = require("await")
 
+function doStuff(){
+	return await('one');
+	var black = "black"
+	return black
+}
+function doStuff1(){
+	return await('two');
+	var jack = "jack";
+	console.log(jack);
+}
+
+doStuff().then(function(){
+	return doStuff1()
+}).then(function(got){
+	console.log(got.one)
+	console.log(got.two)
+}).catch((err) => {
+	console.error(err)
+})
